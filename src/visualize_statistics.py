0306e052192963e84ba1b843e203b111766434f6
tbarbierinewf <tbarbieri@newfcap.com>
2024-01-05 06:46:14 -0300

    added colors features

--- src/visualize_statistics.py
+++ src/visualize_statistics.py
@@ -1,12 +1,21 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 def plot(statistics, batch_per_epoca, learning_rate):
     df = pd.read_json(statistics).transpose()
 
     plt.figure(figsize=(10, 6))
-    for column in df.columns:
-        plt.plot(df.index, df[column], label=column)
+
+    # Plot the first three columns with hot colors
+    hot_colors = plt.cm.hot(np.linspace(0.2, 0.8, 3))
+    for i, column in enumerate(df.columns[:3]):
+        plt.plot(df.index, df[column], label=column, color=hot_colors[i])
+
+    # Plot the rest of the columns with cold colors
+    cold_colors = plt.cm.cool(np.linspace(0.2, 0.8, len(df.columns) - 3))
+    for i, column in enumerate(df.columns[3:]):
+        plt.plot(df.index, df[column], label=column, color=cold_colors[i])
 
     plt.xlabel('Epoch')
     plt.ylabel('Loss')

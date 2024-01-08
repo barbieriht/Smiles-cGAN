6eee3f3a7d371f4f6333c996c1b62c682fccc855
tbarbierinewf <tbarbieri@newfcap.com>
2024-01-08 09:32:29 -0300

    added colors feature

--- src/visualize_statistics.py
+++ src/visualize_statistics.py
@@ -8,12 +8,12 @@ def plot(statistics, batch_per_epoca, learning_rate):
     plt.figure(figsize=(10, 6))
 
     # Plot the first three columns with hot colors
-    hot_colors = plt.cm.hot(np.linspace(0.2, 0.8, 3))
+    hot_colors = plt.cm.hot(np.linspace(0.25, 0.75, 3))
     for i, column in enumerate(df.columns[:3]):
         plt.plot(df.index, df[column], label=column, color=hot_colors[i])
 
     # Plot the rest of the columns with cold colors
-    cold_colors = plt.cm.cool(np.linspace(0.2, 0.8, len(df.columns) - 3))
+    cold_colors = plt.cm.cool(np.linspace(0.25, 0.75, len(df.columns) - 3))
     for i, column in enumerate(df.columns[3:]):
         plt.plot(df.index, df[column], label=column, color=cold_colors[i])
 
@@ -22,4 +22,4 @@ def plot(statistics, batch_per_epoca, learning_rate):
     plt.title('Losses Over Epochs')
     plt.legend()
     
-    plt.savefig(f'./generated_files/bpe{batch_per_epoca}_lr{learning_rate}.png')
+    plt.savefig(f'./generated_files/lr{learning_rate}_bpe{batch_per_epoca}/plot.png')

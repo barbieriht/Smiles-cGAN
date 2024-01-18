98c402dd7b18d0e9327443b5a3cf13da0bd716c7
tbarbierinewf <tbarbieri@newfcap.com>
2024-01-18 19:24:43 -0300

    automated colors

--- src/visualize_statistics.py
+++ src/visualize_statistics.py
@@ -7,15 +7,14 @@ def plot(statistics, path_to_save):
 
     plt.figure(figsize=(10, 6))
 
-    # Plot the first three columns with hot colors
-    hot_colors = plt.cm.hot(np.linspace(0.25, 0.75, 3))
-    for i, column in enumerate(df.columns[:3]):
-        plt.plot(df.index, df[column], label=column, color=hot_colors[i])
+    hot_colors = plt.cm.hot(np.linspace(0.25, 0.75, len(df.columns)))
+    cold_colors = plt.cm.cool(np.linspace(0.25, 0.75, len(df.columns)))
 
-    # Plot the rest of the columns with cold colors
-    cold_colors = plt.cm.cool(np.linspace(0.25, 0.75, len(df.columns) - 3))
-    for i, column in enumerate(df.columns[3:]):
-        plt.plot(df.index, df[column], label=column, color=cold_colors[i])
+    for i, column in enumerate(df.columns):
+        if "G_" in column:
+            plt.plot(df.index, df[column], label=column, color=hot_colors[i])
+        else:
+            plt.plot(df.index, df[column], label=column, color=cold_colors[i])
 
     plt.xlabel('Epoch')
     plt.ylabel('Loss')
